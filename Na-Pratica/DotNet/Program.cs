e7dc72abe61da6868416c60a0d04891780d647ed
dvFelipe <contato.dvfelipe@gmail.com>
2022-10-04 22:57:48 -0300

    Operadores Condicionais

--- Na-Pratica/DotNet/Program.cs
+++ Na-Pratica/DotNet/Program.cs
@@ -1,13 +1,116 @@
 ﻿using Usando_a_Classe.Models;
 
+
+
+
+//////////////////////////////////////////////////////
+// Aprendendo o switch case | Aula 5 | Operadores Condicionais
+Console.WriteLine("Digite uma letra");
+string? letra = Console.ReadLine();
+
+
+switch (letra)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("Vogal");
+        break;
+
+    default:
+        Console.WriteLine("Não é uma vogal");
+        break;
+}
+
+
+
+// if (letra == "a" ||
+//     letra == "e" ||
+//     letra == "i" ||
+//     letra == "o" ||
+//     letra == "u" )
+// {
+
+//     Console.WriteLine("Vogal");
+
+// }
+// else
+// {
+//     Console.WriteLine("Não é uma Vogal");
+// }
+
+
+
+// Console.WriteLine("Digite uma letra");
+
+// string letra = Console.ReadLine();
+
+// if (letra == "a"){
+
+//     Console.WriteLine("Vogal");
+
+// }else if (letra == "e"){
+
+//     Console.WriteLine("Vogal");
+
+// }else if (letra == "i"){
+
+//     Console.WriteLine("Vogal");
+
+// }else if (letra == "o"){
+
+//     Console.WriteLine("Vogal");
+
+// }else if (letra == "u"){
+
+//     Console.WriteLine("Vogal");
+
+// }else {
+//     Console.WriteLine("Não é uma vogal");
+// }
+
+
+
+///////////////////////////////////////
+// if Aninhado | Aula 4 | Operadores Condicionais|
+// int quantidadeEmEstoque = 10;
+// int quantidadeCompra = 0;
+// bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
+
+
+// Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+// Console.WriteLine($"Quantidade de compra: {quantidadeCompra}");
+// Console.WriteLine($"É possivel realizar a venda? {possivelVenda}");
+
+
+// if (quantidadeCompra == 0){
+
+//     Console.WriteLine("Venda inválida");
+    
+//     }else if (possivelVenda){
+
+//     Console.WriteLine("Venda realizada");
+
+// }else {
+
+//     Console.WriteLine("Desculpe não temos estoque");
+
+// }
+
+
+
+
+
 //Convertendo de Maneira Segura | Aula 8
-string a = "15-";
-int b = 0;
+// string a = "15-";
+// int b = 0;
 
-int.TryParse(a, out b);
+// int.TryParse(a, out b);
 
-Console.WriteLine(b);
-Console.WriteLine("Convertido com sucesso!");
+// Console.WriteLine(b);
+// Console.WriteLine("Convertido com sucesso!");
 
 
 
